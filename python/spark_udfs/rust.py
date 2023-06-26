1c4724e0c8c634fa3aaab2d844399c3403490fff
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2023-06-26 20:40:51 +0300

    Add arrow version of rust udf

--- python/spark_udfs/rust.py
+++ python/spark_udfs/rust.py
@@ -1,8 +1,14 @@
-from pyspark.sql.functions import udf
+import pandas as pd
+from pyspark.sql.functions import udf, pandas_udf
 
 from spark_rust_udfs import sqrt_and_mol
 
 
 @udf("float")
-def rust_sqrt_and_mol(value: int) -> float:
+def rust_sqrt_and_mol_udf(value: int) -> float:
     return sqrt_and_mol(value)
+
+
+@pandas_udf("float")
+def rust_sqrt_and_mol_arrow_udf(value: pd.Series) -> pd.Series:
+    return value.apply(sqrt_and_mol)
--- python/test/test_rust_udfs.py
+++ python/test/test_rust_udfs.py
@@ -9,10 +9,19 @@ def spark():
     return SparkSession.builder.appName("pytest-pyspark-local-testing").getOrCreate()
 
 
-def test_rust_sqrt_and_mol(spark: SparkSession):
-    from python.spark_udfs.rust import rust_sqrt_and_mol
+def test_rust_sqrt_and_mol_udf(spark: SparkSession):
+    from python.spark_udfs.rust import rust_sqrt_and_mol_udf
 
     pdf = pd.DataFrame({"value": [1, 4], "expected_res": [43.0, 44.0]})
     df = spark.createDataFrame(pdf)
-    res_df = df.withColumn("res", rust_sqrt_and_mol("value"))
+    res_df = df.withColumn("res", rust_sqrt_and_mol_udf("value"))
+    assert res_df.filter(F.col("res") != F.col("expected_res")).isEmpty()
+
+
+def test_rust_sqrt_and_mol_arrow_udf(spark: SparkSession):
+    from python.spark_udfs.rust import rust_sqrt_and_mol_arrow_udf
+
+    pdf = pd.DataFrame({"value": [1, 4], "expected_res": [43.0, 44.0]})
+    df = spark.createDataFrame(pdf)
+    res_df = df.withColumn("res", rust_sqrt_and_mol_arrow_udf("value"))
     assert res_df.filter(F.col("res") != F.col("expected_res")).isEmpty()
